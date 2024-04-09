32fe61bd0b747e986566801050bceb37a68578ca
Mark Szymczyk <mark@swiftdevjournal.com>
2024-04-09 14:32:09 -0400

    Copy the web view code from Bartleby.

--- HelpBookViewer/WebView.swift
+++ HelpBookViewer/WebView.swift
@@ -5,4 +5,28 @@
 //  Created by mark on 4/9/24.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: NSViewRepresentable {
+    var bookURL: URL
+    var currentChapterFilename: String = ""
+    
+    init(url: URL, filename: String) {
+        self.bookURL = url
+        self.currentChapterFilename = filename
+    }
+    
+    func makeNSView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateNSView(_ nsView: WKWebView, context: Context) {
+        let oebpsFolder = bookURL.appendingPathComponent("OEBPS")
+        let textFolder = oebpsFolder.appendingPathComponent("Text")
+        let chapterFile = textFolder.appendingPathComponent(currentChapterFilename)
+        nsView.loadFileURL(chapterFile, allowingReadAccessTo: oebpsFolder)
+    }
+}
+

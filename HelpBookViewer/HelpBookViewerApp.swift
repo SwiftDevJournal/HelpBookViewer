33b0400839a586e3251c1c34078626d5f06528a9
Mark Szymczyk <mark@swiftdevjournal.com>
2024-04-09 14:28:57 -0400

    Initial Commit

---
+++ HelpBookViewer/HelpBookViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpBookViewerApp.swift
+//  HelpBookViewer
+//
+//  Created by mark on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HelpBookViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
